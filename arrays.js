54f22914b9c1611e4095393246437e8c3994afb9
eyi1 <ej7729@gmail.com>
2018-02-01 00:36:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,8 +6,10 @@ var array = [1]
 function addElementToBeginningOfArray(array, element){
   return array [element,...array]
 }
+addElementToBeginningOfArray ([1], 'foo')
 
 function addElementToBeginningOfArray(array, element){
 return array.unshift(element)
 
 }
+addElementToBeginningOfArray([1],'foo')
