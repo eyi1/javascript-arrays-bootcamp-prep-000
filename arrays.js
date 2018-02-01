e58560293d4adf9e2d066b88d1c19d7ba46be788
eyi1 <ej7729@gmail.com>
2018-02-01 00:52:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,7 +35,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array,element){
 }
 
 function removeElementFromBeginningOfArray (array){
-  array.shift()
+  array.slice(0)
   return array
 }
 
